b010f1e53cc1a34ca35007a3e1f0dfac54e95ccb
Dayo Oliyide <dayo.oliyide@fundingcircle.com>
2015-12-08 11:17:51 +0000

    Fixing a Docker/quay.io issue

--- tag-latest-with-version.sh
+++ tag-latest-with-version.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-secor_version=$(docker inspect  --format='{{.ContainerConfig.Env}}' quay.io/fundingcircle/secor:latest | grep -o "SECOR_VERSION=[0-9.]\+" | cut -d'=' -f2 )
+secor_version=$(docker inspect  --format='{{.ContainerConfig.Env}}' v1.quay.io/fundingcircle/secor:latest | grep -o "SECOR_VERSION=[0-9.]\+" | cut -d'=' -f2 )
 if [[ -z "${secor_version}" ]]; then
     printf "The image was built without SECOR_VERSION environment variable. Thus WON'T tag with a SECOR_VERSION number\n"
 else
     printf "Tagging secor:latest with ${secor_version}\n"
-    docker tag quay.io/fundingcircle/secor:latest quay.io/fundingcircle/secor:${secor_version}
+    docker tag v1.quay.io/fundingcircle/secor:latest v1.quay.io/fundingcircle/secor:${secor_version}
     printf "Pushing quay.io/fundingcircle/secor:${secor_version}\n"
-    docker push quay.io/fundingcircle/secor:${secor_version}  
+    docker push v1.quay.io/fundingcircle/secor:${secor_version}  
 fi
 
